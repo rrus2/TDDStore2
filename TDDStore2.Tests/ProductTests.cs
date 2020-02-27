21b39cfe5983612fa6935ee3ea30963a343ec2cd
Pavel <pavel.ivanko@hotmail.com>
2020-02-27 07:01:52 +0100

    Prepare testing

--- TDDStore2.Tests/ProductTests.cs
+++ TDDStore2.Tests/ProductTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace TDDStore2.Tests
 {
-    public class UnitTest1
+    public class ProductTests
     {
         [Fact]
         public void Test1()
---
+++ TDDStore2.Tests/UserTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDStore2.Tests
+{
+    public class UserTests
+    {
+    }
+}
