33e8988290c1da604b2f3a14887f7d393f8b798a
Pavel <pavel.ivanko@hotmail.com>
2020-02-27 11:36:25 +0100

    Added viewmodels

---
+++ TDDStore2.DataAccess/VIewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TDDStore2.DataAccess.VIewModels
+{
+    public class ProductViewModel
+    {
+        [Required]
+        [Display(Name = "Product name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, double.MaxValue)]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Stock { get; set; }
+        [Required]
+        public int GenreID { get; set; }
+    }
+}
---
+++ TDDStore2.DataAccess/VIewModels/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TDDStore2.DataAccess.VIewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of birth")]
+        public DateTime Birthdate { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Display(Name = "Repeat password")]
+        public string RepeatPassword { get; set; }
+    }
+}
