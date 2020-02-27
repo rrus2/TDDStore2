ef5eeb86f28a422c02e85ea15d19c267c7c6c668
Pavel <pavel.ivanko@hotmail.com>
2020-02-27 06:20:31 +0100

    Started genre service

---
+++ TDDStore2.DataAccess/Services/GenreService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TDDStore2.DataAccess.Models;
+
+namespace TDDStore2.DataAccess.Services
+{
+    public class GenreService : IGenreService
+    {
+        public Task<Genre> CreateGenre(Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGenre(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> GetGenre(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Genre>> GetGenres()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:62710/api/genres");
+                var response = await client.GetAsync(client.BaseAddress);
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception("Error fetching genres");
+                var str = await response.Content.ReadAsStringAsync();
+                var genres = JsonConvert.DeserializeObject<IEnumerable<Genre>>(str);
+                return genres;
+            }
+        }
+
+        public Task<Genre> UpdateGenre(int id, Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDStore2.DataAccess/Services/IGenreService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TDDStore2.DataAccess.Models;
+
+namespace TDDStore2.DataAccess.Services
+{
+    public interface IGenreService
+    {
+        Task<IEnumerable<Genre>> GetGenres();
+        Task<Genre> GetGenre(int id);
+        Task<Genre> CreateGenre(Genre genre);
+        Task<Genre> UpdateGenre(int id, Genre genre);
+        void DeleteGenre(int id);
+    }
+}
