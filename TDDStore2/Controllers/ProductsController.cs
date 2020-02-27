d2300b9ff23f5727efa945fb5bb5d421d7f172bf
Pavel <pavel.ivanko@hotmail.com>
2020-02-27 22:27:13 +0100

    ProductsController front end

---
+++ TDDStore2/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TDDStore2.DataAccess.Services;
+using TDDStore2.DataAccess.VIewModels;
+
+namespace TDDStore2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IGenreService _genreService;
+        public ProductsController(IProductService productService, IGenreService genreService)
+        {
+            _productService = productService;
+            _genreService = genreService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+        public async Task<IActionResult> Create()
+        {
+            await LoadGenres();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductViewModel model)
+        {
+            await _productService.CreateProduct(model);
+            return View("ThankYouProductCreate");
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _productService.GetProduct(id);
+            return View(product);
+        }
+        public IActionResult ThankYouProductCreate()
+        {
+            return View();
+        }
+        private async Task LoadGenres()
+        {
+            var genres = await _genreService.GetGenres();
+            var list = new SelectList(genres, "GenreID", "Name");
+            ViewBag.Genres = list;
+        }
+    }
+}
--- TDDStore2/Startup.cs
+++ TDDStore2/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TDDStore2.DataAccess.Models;
 using TDDStore2.DataAccess.Services;
 
 namespace TDDStore2
