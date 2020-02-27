24256491ae0c6c188dcc899bf6c78a8702c53099
Pavel <pavel.ivanko@hotmail.com>
2020-02-27 04:39:13 +0100

    Generated controllers

---
+++ TDDStore2.API/Migrations/20200227032950_GenreSeed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TDDStore2.API.Migrations
+{
+    public partial class GenreSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Weapon')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Vehicle')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Book')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Accessories')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Other')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres");
+        }
+    }
+}
