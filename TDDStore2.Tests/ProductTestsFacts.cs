feb9af5d91b52853496ec9be75c84bde22a8b7b0
Pavel <pavel.ivanko@hotmail.com>
2020-02-27 11:48:56 +0100

    Started testing

--- TDDStore2.Tests/ProductTestsFacts.cs
+++ TDDStore2.Tests/ProductTestsFacts.cs
@@ -1,4 +1,8 @@
+using Moq;
 using System;
+using System.Threading.Tasks;
+using TDDStore2.DataAccess.Models;
+using TDDStore2.DataAccess.Services;
 using Xunit;
 
 namespace TDDStore2.Tests
@@ -6,9 +10,10 @@ namespace TDDStore2.Tests
     public class ProductTestsFacts
     {
         [Fact]
-        public void Test1()
+        public void CreateProductSuccess()
         {
-
+            var product = new Product { Name = "AK-47", Price = 2000, Stock = 20, GenreID = 1 };
+            var mock = new Mock<IProductService>().Setup(x => x.CreateProduct(product)).Returns(Task.FromResult(product));
         }
     }
 }
