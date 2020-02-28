0fcfcd227c6119f4e44e0a335bab71a92d0dfe31
Pavel <pavel.ivanko@hotmail.com>
2020-02-28 12:59:38 +0100

    Create product test Fact

--- TDDStore2.Tests/ServiceTests/ProductTestsFacts.cs
+++ TDDStore2.Tests/ServiceTests/ProductTestsFacts.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TDDStore2.DataAccess.Models;
 using TDDStore2.DataAccess.Services;
@@ -10,31 +12,50 @@ namespace TDDStore2.Tests.ServiceTests
 {
     public class ProductTestsFacts
     {
-        // Create tests
+        private readonly ApplicationDbContext _context;
+        public ProductTestsFacts()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            _context = new ApplicationDbContext(options);
+
+            _context.Database.EnsureCreated();
+        }
+
         [Fact]
         public void CreateProductSuccess()
         {
-            var model = new ProductViewModel { Name = "AK-47", Price = 2000, Stock = 20, GenreID = 1 };
-            var product = new Product { Name = model.Name, Price = model.Price, Stock = model.Stock, GenreID = model.GenreID };
-            var mock = new Mock<IProductService>().Setup(x => x.CreateProduct(model)).Returns(Task.FromResult(product));
+            //act
+            // create genre
+            var genre = new Genre { Name = "Weapon" };
+            _context.Genres.Add(genre);
+            _context.SaveChanges();
+
+            //create product
+            var product1 = new Product { Name = "AK-47", Price = 2000, Stock = 20, GenreID = genre.GenreID };
+            var product2 = new Product { Name = "FN-FAL", Price = 2500, Stock = 15, GenreID = genre.GenreID };
+            _context.Products.Add(product1);
+            _context.Products.Add(product2);
+            _context.SaveChanges();
+
+            //arrange
+            var list = _context.Products.ToList();
+
+            //assert
+            Assert.Equal(2, list.Count);
+            Dispose();
+            
         }
-        [Fact]
-        public void CreateEmptyProductFail()
+
+
+
+
+
+        public void Dispose()
         {
-            var model = new ProductViewModel();
-            var mock = new Mock<IProductService>().Setup(x => x.CreateProduct(model)).Throws(new Exception());
-        }
-        [Fact]
-        public void CreateProductWithoutGenreFails()
-        {
-            var model = new ProductViewModel { Name = "AK-47", Price = 2000, Stock = 20 };
-            var mock = new Mock<IProductService>().Setup(x => x.CreateProduct(model)).Throws(new Exception());
-        }
-        [Fact]
-        public void CreateProductWithNegativePriceFails()
-        {
-            var model = new ProductViewModel { Name = "AK-47", Price = -1, Stock = 20 };
-            var mock = new Mock<IProductService>().Setup(x => x.CreateProduct(model)).Throws(new Exception());
+            _context.Database.EnsureDeleted();
+
+            _context.Dispose();
         }
     }
 }
