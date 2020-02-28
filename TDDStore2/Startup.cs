e9faf4a410a57d744ff1309100c46544127fa07d
Pavel <pavel.ivanko@hotmail.com>
2020-02-28 02:12:10 +0100

    Finally fixed authentication

--- TDDStore2/Startup.cs
+++ TDDStore2/Startup.cs
@@ -56,6 +56,7 @@ namespace TDDStore2
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
